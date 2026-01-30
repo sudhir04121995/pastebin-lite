15ed450af147a2ad89590c3c9ff31b1bf0111eab
Sudharsanan <mechsudharsanan555@gmail.com>
2026-01-30 13:10:36 +0530

    versal url is changed

--- components/CreatePasteForm.tsx
+++ components/CreatePasteForm.tsx
@@ -91,7 +91,7 @@ export default function CreatePasteForm() {
             }}
             className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 text-sm"
           >
-            📋 Copy URL
+            📋  Copy THE URL
           </button>
         </div>
       )}
